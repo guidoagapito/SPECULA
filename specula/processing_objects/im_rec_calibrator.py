5b987960fd8d36be9bde5736e44969a4fd7dc7b1
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-01-14 11:25:03 +0100

    Added first_mode parameter to ImRecCalibrator

--- specula/processing_objects/im_rec_calibrator.py
+++ specula/processing_objects/im_rec_calibrator.py
@@ -13,6 +13,7 @@ class ImRecCalibrator(BaseProcessingObj):
                  data_dir: str,         # Set by main simul object
                  rec_tag: str,
                  im_tag: str = None,
+                 first_mode: int = 0,
                  pupdata_tag: str = None,
                  tag_template: str = None,
                  target_device_idx: int = None, 
@@ -20,6 +21,7 @@ class ImRecCalibrator(BaseProcessingObj):
                 ):
         super().__init__(target_device_idx=target_device_idx, precision=precision)        
         self._nmodes = nmodes
+        self._first_mode = first_mode
         self._data_dir = data_dir
         if tag_template is None and (rec_tag is None or rec_tag == 'auto'):
             raise ValueError('At least one of tag_template and rec_tag must be set')
@@ -30,7 +32,6 @@ class ImRecCalibrator(BaseProcessingObj):
             self._rec_filename = rec_tag
         self._im_filename = im_tag
         self._im = None
-        
         self.inputs['in_slopes'] = InputValue(type=Slopes)
         self.inputs['in_commands'] = InputValue(type=BaseValue)
 
@@ -57,8 +58,7 @@ class ImRecCalibrator(BaseProcessingObj):
         idx = self.xp.nonzero(commands)
 
         if len(idx[0])>0:
-            mode = int(idx[0])
-            print(mode, self._im.shape)
+            mode = int(idx[0]) - self._first_mode
             if mode < self._nmodes:
                 self._im[mode] += slopes / commands[idx]
         
@@ -73,6 +73,8 @@ class ImRecCalibrator(BaseProcessingObj):
     def finalize(self):
         im = Intmat(self._im, pupdata_tag = self.pupdata_tag,
                     target_device_idx=self.target_device_idx, precision=self.precision)
+
+        # TODO add to IM the information about the first mode
         if self._im_filename:
             im.save(os.path.join(self._data_dir, self._im_filename))
         if self._rec_filename:
