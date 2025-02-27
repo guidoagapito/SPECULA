d13e23cb1731c6de165164b913a9069a29657e5b
Guido Agapito <guido.agapito@inaf.it>
2025-02-27 10:33:27 +0100

    (GA) butterworth low pass filter

--- specula/processing_objects/low_pass_filter.py
+++ specula/processing_objects/low_pass_filter.py
@@ -4,15 +4,22 @@ from specula.data_objects.iir_filter_data import IIRFilterData
 
     
 class LowPassFilter(IIRFilter):
-    def __init__(self, cutoff_freq, amplif_fact, time_step, delay=0, offset=None, og_shaper=None,                 
+    def __init__(self, cutoff_freq, time_step, amplif_fact=None, n_ord=None,
+                delay=0, offset=None, og_shaper=None,                 
                 target_device_idx=None, 
                 precision=None
                 ):
         
         samp_freq = 1 / time_step
         
-        iir_filter_data = IIRFilterData.from_fc_and_ampl(cutoff_freq, amplif_fact,
+        if amplif_fact is not None:
+            if n_ord is not None:
+                raise ValueError('Only one of amplif_fact and n_ord can be specified')
+            iir_filter_data = IIRFilterData.lpf_from_fc_and_ampl(cutoff_freq, amplif_fact,
                                                samp_freq, target_device_idx=target_device_idx)
+        else:
+            iir_filter_data = IIRFilterData.lpf_from_fc(cutoff_freq, samp_freq, n_ord=n_ord,
+                                                        target_device_idx=target_device_idx)
 
         # Initialize IIRFilter object
         super().__init__(iir_filter_data, delay=delay, offset=offset, og_shaper=og_shaper,
