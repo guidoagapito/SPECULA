160832781bc3a8eab0cebe6ae89688ce34138b56
FabioRossiArcetri <rossi76@gmail.com>
2025-05-06 16:41:52 +0200

    (FRO) WIP versions of propagation and model analysis with per-layer r… (#96)
    * (FRO) WIP versions of propagation and model analysis with per-layer results to perform multi-layer calibrations.
    
    * Update atmo_propagation.py
    
    * Update modal_analysis.py, making in_ef and in_ef_list optional, since one or the other can be omitted
    
    * Update modal_analysis.py, fixing bug related to missing target_device_idx in get call
    
    * Update modal_analysis.py
    
    * Update modal_analysis.py
    
    * Update modal_analysis.py
    
    * Update modal_analysis.py

--- specula/processing_objects/modal_analysis.py
+++ specula/processing_objects/modal_analysis.py
@@ -1,6 +1,6 @@
 from specula.base_processing_obj import BaseProcessingObj
 from specula.base_value import BaseValue
-from specula.connections import InputValue
+from specula.connections import InputValue, InputList
 from specula.data_objects.electric_field import ElectricField
 from specula.data_objects.ifunc import IFunc
 from specula.data_objects.ifunc_inv import IFuncInv
@@ -52,12 +52,14 @@ class ModalAnalysis(BaseProcessingObj):
             # Prioritize ifunc_inv
             self.phase2modes = ifunc_inv
 
+        self.ifunc = ifunc
         self.rms = BaseValue('modes', 'output RMS of modes from modal reconstructor') 
         self.dorms = dorms
         self.wavelengthInNm = wavelengthInNm
         self.verbose = False  # Verbose flag for debugging output
-        self.out_modes = BaseValue('output modes from modal analysis', target_device_idx=target_device_idx)        
-        self.inputs['in_ef'] = InputValue(type=ElectricField)
+        self.out_modes = BaseValue('output modes from modal analysis', target_device_idx=target_device_idx)
+        self.inputs['in_ef'] = InputValue(type=ElectricField, optional=True)
+        self.inputs['in_ef_list'] = InputList(type=ElectricField, optional=True)
         self.outputs['out_modes'] = self.out_modes
         self.outputs['rms'] = self.rms
 
@@ -65,6 +67,7 @@ class ModalAnalysis(BaseProcessingObj):
     def prepare_trigger(self, t):
         super().prepare_trigger(t)
         self.in_ef = self.local_inputs['in_ef']
+        self.in_ef_list = self.local_inputs['in_ef_list']
 
 
     def unwrap_2d(self, p):
@@ -77,8 +80,21 @@ class ModalAnalysis(BaseProcessingObj):
             unwrapped_p[c, :] = self.xp.unwrap(col)        
         return unwrapped_p
 
+    def setup(self, loop_dt, loop_niters):
+        super().setup(loop_dt, loop_niters)
+        input_list = self.inputs['in_ef_list'].get(self.target_device_idx)
+        if input_list:
+            for i in range(len(input_list)):
+                self.outputs['out_modes_list'].append(BaseValue('modes', target_device_idx=self.target_device_idx))
+            self.out_modes_list = self.outputs['out_modes_list']
 
     def trigger_code(self):
+        if self.in_ef:
+            ef_list = [self.in_ef]
+            output_list = [self.out_modes]
+        else:
+            ef_list = self.in_ef_list
+            output_list = self.out_modes_list
         if self.phase2modes._doZeroPad:
             m = self.xp.dot(self.in_ef.phaseInNm, self.phase2modes.ifunc_inv)
         else:
@@ -96,6 +112,24 @@ class ModalAnalysis(BaseProcessingObj):
         self.out_modes.value = m
         self.out_modes.generation_time = self.current_time
 
+        for li, current_ef in enumerate(ef_list):
+            if self.ifunc._doZeroPad:
+                m = self.xp.dot(current_ef.phaseInNm, self.phase2modes.ifunc_inv)
+            else:
+                if self.wavelengthInNm > 0:
+                    phase_in_rad = current_ef.phaseInNm * (2 * self.xp.pi / self.wavelengthInNm)
+                    phase_in_rad *= self.ifunc.mask_inf_func.astype(float)
+                    phase_in_rad = self.unwrap_2d(phase_in_rad)
+                    phase_in_nm = phase_in_rad * (self.wavelengthInNm / (2 * self.xp.pi))
+                    ph = phase_in_nm[self.ifunc.idx_inf_func]
+                else:
+                    ph = current_ef.phaseInNm[self.ifunc.idx_inf_func]
+
+                m = self.xp.dot(ph, self.phase2modes.ifunc_inv)
+            
+            output_list[li].value = m
+            output_list[li].generation_time = self.current_time
+        
         if self.dorms:
             self.rms.value = self.xp.std(ph)
             self.rms.generation_time = self.current_time
