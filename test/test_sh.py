dd4a4d3c916fac1379d78a46047d59374ae77a45
Alfio Puglisi <alfio.puglisi@inaf.it>
2025-05-26 12:03:41 +0200

    Reverted last two commits

--- test/test_sh.py
+++ test/test_sh.py
@@ -1,12 +1,9 @@
 
 
 import specula
-from specula.data_objects.pupilstop import Pupilstop
-from specula.data_objects.simul_params import SimulParams
 specula.init(0)  # Default target device
 
 import unittest
-from scipy.ndimage import rotate
 
 from specula import cp, np
 from specula import cpuArray
@@ -143,83 +140,3 @@ class TestSH(unittest.TestCase):
         
         assert id(sh1._wf3) == id(sh2._wf3) 
         assert id(sh1._wf3) != id(sh3._wf3) 
-
-    @cpu_and_gpu
-    def test_sh_rotation(self, target_device_idx, xp):
-        '''
-        Test that input EF rotation is correctly handled, by comparing
-        the SH output with a non-rotated EF input plus a rotation parameter,
-        and a rotated EF without the rotatio parameter
-        '''
-        t = 1
-        pxscale_arcsec = 0.5
-        pixel_pupil = 120
-        pixel_pitch = 0.05
-        sh_npix = 6
-        rotAnglePhInDeg = 42.
-
-        simul_params = SimulParams(pixel_pitch=pixel_pitch, pixel_pupil=pixel_pupil)
-        sh_non_rotated = SH(wavelengthInNm=500,
-                            subap_wanted_fov= sh_npix * pxscale_arcsec,
-                            sensor_pxscale=pxscale_arcsec,
-                            subap_on_diameter=20,
-                            subap_npx=sh_npix,
-                            rotAnglePhInDeg=0,
-                            target_device_idx=target_device_idx)
-
-        sh_rotated = SH(wavelengthInNm=500,
-                        subap_wanted_fov= sh_npix * pxscale_arcsec,
-                        sensor_pxscale=pxscale_arcsec,
-                        subap_on_diameter=20,
-                        subap_npx=sh_npix,
-                        rotAnglePhInDeg=rotAnglePhInDeg,
-                        target_device_idx=target_device_idx)
-
-        # tilt corresponding to pxscale_arcsec
-        tilt_value = np.radians(pixel_pupil * pixel_pitch * 1/(60*60) * pxscale_arcsec)
-        tilt = np.linspace(-tilt_value / 2, tilt_value / 2, pixel_pupil)
-
-        # Tilted wavefront, non-rotated
-        ef_non_rotated = ElectricField(pixel_pupil, pixel_pupil, pixel_pitch, S0=1, target_device_idx=target_device_idx)
-        ef_non_rotated.phaseInNm[:] = xp.array(np.broadcast_to(tilt, (pixel_pupil, pixel_pupil))) * 1e9
-        ef_non_rotated.generation_time = t
-
-        ef_rotated = ElectricField(pixel_pupil, pixel_pupil, pixel_pitch, S0=1, target_device_idx=target_device_idx)
-        ef_rotated.phaseInNm[:] = xp.array(rotate(cpuArray(ef_non_rotated.phaseInNm), rotAnglePhInDeg, reshape=False))
-        ef_rotated.generation_time = t
-
-        pupilstop = Pupilstop(simul_params=simul_params, target_device_idx=target_device_idx)
-        ef_non_rotated.A *= pupilstop.A
-        ef_rotated.A *= pupilstop.A        
-
-        sh_non_rotated.inputs['in_ef'].set(ef_rotated)
-        sh_rotated.inputs['in_ef'].set(ef_non_rotated)
-
-        sh_non_rotated.setup()
-        sh_non_rotated.check_ready(t)
-        sh_non_rotated.trigger()
-        sh_non_rotated.post_trigger()
-        i_non_rotated = sh_non_rotated.outputs['out_i'].i
-
-        sh_rotated.setup()
-        sh_rotated.check_ready(t)
-        sh_rotated.trigger()
-        sh_rotated.post_trigger()
-        i_rotated = sh_rotated.outputs['out_i'].i
-
-        abstol = 0.005
-        bad_elements = abs((i_non_rotated - i_rotated)) > abstol
-
-        # import matplotlib.pyplot as plt
-        # plt.figure()
-        # plt.imshow(cpuArray(i_non_rotated))
-        # plt.figure()
-        # plt.imshow(cpuArray(i_rotated))
-        # plt.figure()
-        # plt.imshow(cpuArray(i_non_rotated - i_rotated))
-        # plt.figure()
-        # plt.imshow(cpuArray(bad_elements))
-        # plt.show()
-        
-        # Some pixels on the edges are different, but most are the same
-        assert xp.count_nonzero(bad_elements) < 3
