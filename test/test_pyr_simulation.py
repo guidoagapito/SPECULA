2620f1c77e530fbba32ad34c5fab46736446442d
Guido Agapito <guido.agapito@inaf.it>
2025-07-25 14:08:38 +0200

    (GA) specula init

--- test/test_pyr_simulation.py
+++ test/test_pyr_simulation.py
@@ -3,7 +3,7 @@ import os
 import shutil
 import glob
 import specula
-specula.init(-1,precision=1)  # Default target device
+specula.init(0)
 
 from specula import np
 from specula.simul import Simul
