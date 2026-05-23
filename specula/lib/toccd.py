9d447c2eb2381512cf962323bf0fe090870f505e
Guido Agapito <guido.agapito@inaf.it>
2026-05-23 14:30:51 +0200

    (GA) Fix possible FP64 truncation bug in toccd CUDA kernel (#665)

--- specula/lib/toccd.py
+++ specula/lib/toccd.py
@@ -108,7 +108,6 @@ def toccd_gpu(a, newshape, set_total=None):
     dx_out = int(mcmx / outx)
     dy_out = int(mcmy / outy)
     f = 1.0 / (dx_out * dy_out)
-    oneOverDxIn = 1.0 / dx_in
 
     block = (16, 16)
     numBlocks2d = int(outx // block[1])
@@ -125,10 +124,10 @@ def toccd_gpu(a, newshape, set_total=None):
     out = cp.empty_like(a, shape=(outy, outx))
 
     if a.dtype == cp.float32:
-        _rebin2D_step1_float(grid_tmp, block, (a, tmp, inx, iny, outx, outy, dx_out, cp.float32(oneOverDxIn)))
+        _rebin2D_step1_float(grid_tmp, block, (a, tmp, inx, iny, outx, outy, dx_out, dx_in))
         _rebin2D_step2_float(grid, block, (tmp, out, outx, outy, dy_in, dy_out, cp.float32(f)))
     elif a.dtype == cp.float64:
-        _rebin2D_step1_double(grid_tmp, block, (a, tmp, inx, iny, outx, outy, dx_out, cp.float64(oneOverDxIn)))
+        _rebin2D_step1_double(grid_tmp, block, (a, tmp, inx, iny, outx, outy, dx_out, dx_in))
         _rebin2D_step2_double(grid, block, (tmp, out, outx, outy, dy_in, dy_out, cp.float64(f)))
     else:
         raise TypeError(f'toccd_gpu(): unsupported dtype {a.dtype}. Valid dtypes are float32 and float64')
@@ -147,7 +146,7 @@ if cp:
     kernel_step1 = r'''
 extern "C" __global__
 void rebin2D_step1_TYPE(TYPE *g_in, TYPE *g_tmp, int inx, int iny, int outx, int outy,
-                   int dx_out, TYPE oneOverDxIn) {
+                   int dx_out, int dx_in) {
 
    int y = blockIdx.y * blockDim.y + threadIdx.y;
    int x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -157,11 +156,11 @@ void rebin2D_step1_TYPE(TYPE *g_in, TYPE *g_tmp, int inx, int iny, int outx, int
 
    if ((y<iny) && (x<outx)) {
        i = x*dx_out;
-       prev_pos = i * oneOverDxIn;
+       prev_pos = i / dx_in;
        value = g_in[y*inx + prev_pos];
 
        for ( ; i<(x+1)*dx_out; i++) {
-          pos = i * oneOverDxIn;
+          pos = i / dx_in;
           if (pos != prev_pos) {
              value = g_in[y*inx + pos];
              prev_pos = pos;
